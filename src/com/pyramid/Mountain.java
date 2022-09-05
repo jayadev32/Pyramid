53c17c8216999c26b2c75edb5ca21d5c2673819a
jayadev32 <jayadevan32@gmail.com>
2022-09-05 13:11:10 +0530

    code pulled from local

---
+++ src/com/pyramid/Mountain.java
@@ -0,0 +1,14 @@
+package com.pyramid;
+
+public class Mountain {
+	public static void main(String[] args) {
+		int rows = 10;
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
